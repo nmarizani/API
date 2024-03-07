f2850e44a524cce76e154c86b6f075a13b14cfa0
waka-man <wtwestman@gmail.com>
2024-03-07 10:36:15 +0200

    Prints titles now

--- problem_one.py
+++ problem_one.py
@@ -3,7 +3,7 @@ import requests
 def get_emp_todo(emp_id):
     TODO_URL = f"https://jsonplaceholder.typicode.com/users/{emp_id}/todos"
     USER_URL = f"https://jsonplaceholder.typicode.com/users/{emp_id}"
-
+    
     # Get the JSON of TODOS - DONE
     # Get TOTAL NUMBER of todos - DONE
     # Get COMPLETED NUMBER of todos - DONE
@@ -12,15 +12,22 @@ def get_emp_todo(emp_id):
     # GET THE TITLES OF THE COMPLETED TASKS
     # AND PRINT THEM ALL OUT ON STDOUT
     # EACH TASKS SHOULD BE PRINTED ON A NEW LINE
-    
-    emp_name = requests.get(USER_URL).json()["name"]
+
+
     emp_todos = requests.get(TODO_URL).json()
+    emp_name = requests.get(USER_URL).json()["name"]
     emp_total_todos = len(emp_todos)
     emp_completed_todos = 0
+    todo_titles = []
+
     for todo in emp_todos:
         if todo["completed"] is True:
+            todo_titles.append(todo["title"])
             emp_completed_todos += 1
     print(f"Employee {emp_name} From total todos of {emp_total_todos}, {emp_completed_todos} tasks have been done")
 
+    for todo_title in todo_titles:
+        print(todo_title)
 
-get_emp_todo(3)
+get_emp_todo(1)
+
